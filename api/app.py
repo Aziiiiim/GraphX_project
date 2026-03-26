e1ef202a81b553815972724afe6a4e859bd8ae4e
Paul.E <paul.enjalbert@imt-atlantique.net>
2026-03-26 18:34:45 +0100

    changes to graphx

--- api/app.py
+++ api/app.py
@@ -10,5 +10,9 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/graph")
+def graph_request():
+    return request()
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
---
+++ api/queries/graphx.py
@@ -0,0 +1,17 @@
+from utils.spark_config import sql_context
+
+def request():
+    test()
+    return "<p>Graph</p>"
+
+def test():
+    subway_routes_df = sql_context.sql(
+    """
+        SELECT DISTINCT * FROM routes r
+        WHERE agency_id = "IDFM:Operator_100"  
+            AND route_type = 1
+    """)
+
+    subway_routes_df.show()
+
+    subway_routes_df.createOrReplaceTempView("subway_routes")
