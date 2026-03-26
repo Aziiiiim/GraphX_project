0326c1a1e9fa034cb9b5bb0211aefa125e3d1478
Fl0r3ntLem <folem0302@gmail.com>
2026-03-26 19:03:34 +0100

    split conf

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@ from queries.rdd import *
 from queries.dataframe import *
 from queries.graphx import *
 from queries.streaming import *
-from utils.conf import sql_context, client_minio, consumer_manager
+from utils.conf import client_minio, consumer_manager
 
 app = Flask(__name__)
 
